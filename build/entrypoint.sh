bc0b5720e9537e238ff364994835f24de8b68d72
Eduardo Antuña Díez <eduadiez@gmail.com>
2019-11-22 16:14:30 +0100

    Add compatibility with the regular trustline node

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -7,6 +7,22 @@ if [ "${NETSTATS_ENABLE}" == "no" ]; then
     exit 0;
 fi
 
+echo "Waiting for the node to start and the DNS is updated..."
+# Delay to wait until the node is started and the DNS is updated
+sleep 30
+
+VALIDATOR_IP=$(dig +short trustlines-validator.dappnode)
+NODE_IP=$(dig +short trustlines.dappnode)
+
+if [ ! -z "$VALIDATOR_IP" ]; then
+    RPC_HOST=trustlines-validator.dappnode
+elif [ ! -z "$NODE_IP" ]; then
+    RPC_HOST=trustlines.dappnode
+else 
+    echo "Not found a valid RPC endpoint"
+    exit 1
+fi
+
 if [ "${SHOW_VALIDATOR_STATUS}" == "yes" ];then
     HIDE_VALIDATOR_STATUS=false /src/start.sh
 else
